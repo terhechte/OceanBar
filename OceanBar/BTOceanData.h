bdfd656c8ace97bacf66cdd81cb0f6d064c4a44e
Benedikt Terhechte <terhechte@gmail.com>
2014-04-28 20:44:00 +0200

    Simple model geared towards the Digital Ocean API. The objects revolve around the original dictionary being distributed by DO, but they add additional convenience conversion to native Objective-C objects, where necessary, and convert ids into the correct objects for easier usage.

--- OceanBar/BTOceanData.h
+++ OceanBar/BTOceanData.h
@@ -8,6 +8,51 @@
 
 #import <Foundation/Foundation.h>
 
-@interface BTOceanData : NSObject
+typedef void (^BTOceanDataAction)(id results);
+typedef void (^BTOceanDataError)(NSError *error);
 
+//-----------------------------------------------------------------------------
+#pragma mark Types
+//-----------------------------------------------------------------------------
+// Lightweight interfaces-types around the few properties
+// that the items contain
+
+@interface BTOceanDataItem : NSObject
+- (id) initWithDictionary:(NSDictionary *)otherDictionary;
+- (id)objectForKeyedSubscript:(id <NSCopying>)key;
+- (NSNumber*) identifier;
+- (NSString*) name;
+- (NSString*) slug;
 @end
+
+@interface BTOceanDataImage : BTOceanDataItem
+- (NSString*) distribution;
+@end
+
+@interface BTOceanDataSize : BTOceanDataItem
+- (NSNumber*) memory;
+- (NSNumber*) disk;
+- (NSNumber*) cpu;
+- (NSNumber*) costPerHour;
+- (NSNumber*) costPerMonth;
+@end
+
+@interface BTOceanDataRegion : BTOceanDataItem
+@end
+
+@interface BTOceanDataDroplet : BTOceanDataItem
+@property (retain) BTOceanDataSize *size;
+@property (retain) BTOceanDataRegion *region;
+@property (retain) BTOceanDataImage *image;
+- (bool) backupsActive;
+- (NSString*) ipAddress;
+- (NSString*) privateIpAddress;
+- (bool) locked;
+- (NSString*) status;
+- (NSDate*) createdAt;
+@end
+
+@interface BTOceanData : NSObject
+- (void) loadDropletsWithSuccess:(BTOceanDataAction)actionBlock failure:(BTOceanDataError)errorBlock;
+@end
+
--- OceanBar/BTOceanData.m
+++ OceanBar/BTOceanData.m
@@ -7,7 +7,238 @@
 //
 
 #import "BTOceanData.h"
+#import <AFNetworking/AFNetworking.h>
+
+@interface BTOceanDataItem() {
+    NSDictionary *_storage;
+}
+
+@end
+
+@implementation BTOceanDataItem
+
+- (id) initWithDictionary:(NSDictionary *)otherDictionary {
+    self = [super init];
+    if (self) {
+        _storage = otherDictionary;
+    }
+    return self;
+}
+
+- (id)objectForKeyedSubscript:(id <NSCopying>)key {
+    return [_storage objectForKey:key];
+}
+
+- (NSNumber*) identifier {
+    return self[@"id"];
+}
+
+- (NSString*) name {
+    return self[@"name"];
+}
+
+- (NSString*) slug {
+    return self[@"slug"];
+}
+
+@end
+
+@implementation BTOceanDataImage
+
+- (NSString*) distribution {
+    return self[@"distribution"];
+}
+
+@end
+
+@implementation BTOceanDataRegion
+@end
+
+@implementation BTOceanDataSize
+
+- (NSNumber*) memory {
+    return self[@"memory"];
+}
+- (NSNumber*) disk {
+    return self[@"disk"];
+}
+
+- (NSNumber*) cpu {
+    return self[@"cpu"];
+}
+
+- (NSNumber*) costPerHour {
+    return self[@"cost_per_hour"];
+}
+
+- (NSNumber*) costPerMonth {
+    return self[@"cost_per_month"];
+}
+
+@end
+
+@implementation BTOceanDataDroplet
+
+- (bool) backupsActive {
+    return [self[@"backups_active"] boolValue];
+}
+
+- (NSString*) ipAddress {
+    return self[@"ip_address"];
+}
+
+- (NSString*) privateIpAddress {
+    return self[@"private_ip_address"];
+}
+
+- (bool) locked {
+    return [self[@"locked"] boolValue];
+}
+
+- (NSString*) status {
+    return self[@"status"];
+}
+
+- (NSDate*) createdAt {
+    // 2013-01-01T09:30:00Z
+    NSString *dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    NSDateFormatter *dateFormatter =
+    [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:dateFormat];
+    return [dateFormatter dateFromString:self[@"created_at"]];
+}
+
+@end
+
+@interface BTOceanData()
+{
+    NSDictionary *_images;
+    NSDictionary *_regions;
+    NSDictionary *_sizes;
+}
+@end
 
 @implementation BTOceanData
+
+- (NSString*) authURLString {
+    return $p(@"?client_id=%@&api_key=%@", DIGITALOCEAN_CLIENT_ID, DIGITALOCEAN_API_KEY);
+}
+
+- (NSString*) dropletURLString {
+    return $p(@"%@/droplets/%@", DIGITALOCEAN_BASE_URL, [self authURLString]);
+}
+
+- (NSString*) regionURLString {
+    return $p(@"%@/regions/%@", DIGITALOCEAN_BASE_URL, [self authURLString]);
+}
+
+- (NSString*) imageURLString {
+    return $p(@"%@/images/%@", DIGITALOCEAN_BASE_URL, [self authURLString]);
+}
+
+- (NSString*) sizeURLString {
+    return $p(@"%@/sizes/%@", DIGITALOCEAN_BASE_URL, [self authURLString]);
+}
+
+- (void) loadDropletsWithSuccess:(BTOceanDataAction)actionBlock failure:(BTOceanDataError)errorBlock {
+    // Each time we reload, we also reload the available
+    // Regions, Images, and Sizes
+    
+    AFHTTPRequestOperation *op1 =
+    [self requestOperationFor:[self sizeURLString]
+                     createClass:@"BTOceanDataSize"
+                     propertyKey:@"sizes"
+                    successBlock:^(id results) {
+                        _regions = results;
+                    } failBlock:^(NSError *error) {
+                        NSLog(@"Error: %@", error);
+                        errorBlock(error);
+                    }];
+    
+    AFHTTPRequestOperation *op2 =
+    [self requestOperationFor:[self regionURLString]
+                     createClass:@"BTOceanDataRegion"
+                     propertyKey:@"regions"
+                    successBlock:^(id results) {
+                        _regions = results;
+                    } failBlock:^(NSError *error) {
+                        NSLog(@"Error: %@", error);
+                        errorBlock(error);
+                    }];
+    
+    AFHTTPRequestOperation *op3 =
+    [self requestOperationFor:[self imageURLString]
+                     createClass:@"BTOceanDataImage"
+                     propertyKey:@"images"
+                    successBlock:^(id resultDict) {
+                        _images = resultDict;
+                    } failBlock:^(NSError *error) {
+                        NSLog(@"Error: %@", error);
+                        errorBlock(error);
+                    }];
+    
+    AFHTTPRequestOperation *finalOp =
+    [self requestOperationFor:[self dropletURLString]
+                     createClass:@"BTOceanDataDroplet"
+                     propertyKey:@"droplets"
+                    successBlock:^(id results) {
+                        // add regions and images
+                        for (BTOceanDataDroplet *droplet in (NSArray*)results) {
+                            droplet.region = _regions[droplet[@"region_id"]];
+                            droplet.size = _sizes[droplet[@"size_id"]];
+                            droplet.image = _images[droplet[@"image_id"]];
+                        }
+                        actionBlock(results);
+                    } failBlock:^(NSError *error) {
+                        errorBlock(error);
+                    }];
+    
+    [finalOp addDependency:op1];
+    [finalOp addDependency:op2];
+    [finalOp addDependency:op3];
+    
+    NSOperationQueue *operationQueue = [[NSOperationQueue alloc] init];
+    operationQueue.maxConcurrentOperationCount = 1;
+    [operationQueue addOperations:@[op1, op2, op3, finalOp]
+                                 waitUntilFinished:NO];
+}
+
+- (AFHTTPRequestOperation*) requestOperationFor:(NSString*)urlString
+                    createClass:(NSString*)createClass
+                    propertyKey:(NSString*)propertyKey
+                   successBlock:(BTOceanDataAction)success
+                      failBlock:(BTOceanDataError)failBlock {
+    
+    NSURL *URL = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    AFHTTPRequestOperation *op = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    op.responseSerializer = [AFJSONResponseSerializer serializer];
+    [op setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        // Never know what we get back in JSON
+        if (![responseObject isKindOfClass:[NSDictionary class]]) {
+            failBlock(errorFor(1, $p(@"Error: ImageData is '%@ class'", [responseObject className]), @"OceanData"));
+            return;
+        }
+        
+        if (![[(NSDictionary*)responseObject objectForKey:@"status"] isEqualToString:@"OK"]) {
+            failBlock(errorFor(1, $p(@"Status not 'OK' for '%@'", propertyKey), @"OceanData"));
+            return;
+        }
+        
+        NSMutableDictionary *collector = [NSMutableDictionary dictionary];
+        for (NSDictionary *aDictionary in responseObject[propertyKey]) {
+            BTOceanDataItem *item = [[NSClassFromString(createClass) alloc] initWithDictionary:aDictionary];
+            
+            [collector setObject:item forKey:item.identifier];
+        }
+        
+        success(collector.copy);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        failBlock(error);
+    }];
+    
+    return op;
+}
 
 @end
